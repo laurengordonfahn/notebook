f7643dd9e4831aa3d9a1a99e13a80c00dfe92cf2
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-01-16 20:15:43 -0800

    placed all of my javascript file in to document.ready function so only runs when accounted for

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,64 +1,65 @@
-//// Update the DOM with new note /////
-function upDateNotes(response){
-
-    var add_note_form = 
-        "<form>" +
-            "<button class=\"delete_note\" value=\""+ response.id +"\"> Delete </button>" +
-            "<div class=\"decorate_note_div\">" +
-
-                "<div>" + response.created_at +"</div>" +
-                "<div>" + response.title + "</div>" +
-                "<div>" +
-                    "<p>" + response.content + "</p>" +
+$(document).ready(function(){
+    //// Update the DOM with new note /////
+    function upDateNotes(response){
+    
+        var add_note_form = 
+            "<form>" +
+                "<button class=\"delete_note\" value=\""+ response.id +"\"> Delete  </button>" +
+                "<div class=\"decorate_note_div\">" +
+    
+                    "<div>" + response.created_at +"</div>" +
+                    "<div>" + response.title + "</div>" +
+                    "<div>" +
+                        "<p>" + response.content + "</p>" +
+                    "</div>" +
                 "</div>" +
-            "</div>" +
-        "</form>";
-
-    $(".new_updated_notes").prepend(add_note_form);
-}
-
-
-function addNewNoteToDB(event){
-
-    var form = $(this).closest("form");
-        var note_title = form.find("[name = \"note_title\"]").val();
-    var new_note = form.find("[name = \"new_note\"]").val();
-
-
-    var formInputs = {
-        "note_title": note_title,
-        "new_note": new_note
-    };
-
-    $.post("/add_note.json", formInputs, upDateNotes);
-}
-
-
-$("#new_note_button").on('click', addNewNoteToDB);
-
-
-
-
-///// Remove note from list of notes //////
-function removeNote(response){
+            "</form>";
     
-    console.log("removeNote running");
+        $(".new_updated_notes").prepend(add_note_form);
+    }
     
-}
-function removeNoteFromDB(){
-    var note_id = $(this).val();
-    $(this).closest('form').remove();
-
-    // $.post('/delete_note', {'note_id': note_id}, removeNote);
-    $.ajax({
-        url: '/delete_note',
-        type: 'DELETE',
-        data: {'note_id': note_id},
-        success: removeNote
-    })
-
-}
-
-$(".delete_note").on('click', removeNoteFromDB);
-
+    
+    function addNewNoteToDB(event){
+    
+        var form = $(this).closest("form");
+            var note_title = form.find("[name = \"note_title\"]").val();
+        var new_note = form.find("[name = \"new_note\"]").val();
+    
+    
+        var formInputs = {
+            "note_title": note_title,
+            "new_note": new_note
+        };
+    
+        $.post("/add_note.json", formInputs, upDateNotes);
+    }
+    
+    
+    $("#new_note_button").on('click', addNewNoteToDB);
+    
+    
+    
+    
+    ///// Remove note from list of notes //////
+    function removeNote(response){
+        
+        console.log("removeNote running");
+        
+    }
+    function removeNoteFromDB(){
+        var note_id = $(this).val();
+        $(this).closest('form').remove();
+    
+        // $.post('/delete_note', {'note_id': note_id}, removeNote);
+        $.ajax({
+            url: '/delete_note',
+            type: 'DELETE',
+            data: {'note_id': note_id},
+            success: removeNote
+        })
+    
+    }
+    
+    $(".delete_note").on('click', removeNoteFromDB);
+});
 
