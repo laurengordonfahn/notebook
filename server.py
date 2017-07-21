a080144d9a63faadd7bdb80910bacd290a00e005
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2017-07-21 09:13:23 -0700

    Spelling Correction
    Landing Page Typo Corrected

--- server.py
+++ server.py
@@ -154,6 +154,6 @@ if __name__ == "__main__":
 
     DebugToolbarExtension(app)
 
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
 
     app.run(host="0.0.0.0", port=port)
